56a750204e85ce85f8e3d45a4930d58002abb48d
Mggebara <mggebara@yahoo.com>
2018-12-04 15:26:20 -0500

    Yesterday's fixes but accurate

--- src/main/java/cs1302/arcade/SudokuGame.java
+++ src/main/java/cs1302/arcade/SudokuGame.java
@@ -1,9 +1,22 @@
 package cs1302.arcade;
 
 import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.HBox;
+import javafx.scene.control.Button;
 
 public class SudokuGame implements Playable {
+    
+    private Stage stage;
+    
     public void play() {
 	
-    }
+	HBox hbox = new HBox(new Button("Test!"));
+	stage = new Stage();
+	Scene scene = new Scene(hbox);
+	stage.setScene(scene);
+	stage.sizeToScene();
+	stage.show();
+	
+    } // play
 }
