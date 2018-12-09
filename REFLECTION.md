c966a982bce6b298a945308b92a55644a4410adc
Mggebara <mggebara@yahoo.com>
2018-12-08 20:20:42 -0500

    Full Sudoku Board and Reflection 2

--- REFLECTION.md
+++ REFLECTION.md
@@ -14,3 +14,11 @@ that the Markdown is transformed into nice looking HTML.
 2. **TODO:** Make the snake move in the Snake game, create food, make snake grow bigger when it eats food, create win and lose conditions, create boundaries that the snake can't pass, do everything for sudoku
 
 3. **PROB:** Changed some code and now the snake won't appear initially when the game starts, when it did before. Also, can't figure out how to make snake move.
+
+## FRE 2018-12-07 @ 9:34 PM EST
+
+1. **DONE:** Create moving snake in Snake game that is able to move around the board in a clean and orderly mannor. Sudoku is now in a possition where the baord is created, and using a predetermined array of String objects, a game of Sudoku can be played. In addition to this, it is possible to check whether each box has one of each number type.
+
+2. **TODO:** Make visible apples that expand the size of the snake, add a score to snake and sudoku, create game boards for Sudoku, stylize the Sudoku game baord, create a win condition for Sudoku, create a lose condition for Snake, create a Game Over screen for snake, and end the game when the snake moves out of bounds.
+
+3. **PROB:** After ending Snake, the code keeps running in the background. In addition to this, it is not currently possible to end an instance of one game, and then play it again after closing it, which is a problem.
--- src/main/java/cs1302/arcade/SudokuGame.java
+++ src/main/java/cs1302/arcade/SudokuGame.java
@@ -6,31 +6,104 @@ import javafx.scene.layout.HBox;
 import javafx.scene.control.Button;
 import java.awt.event.KeyEvent;
 import javafx.scene.input.KeyCode;
+import javafx.scene.paint.Color;
+import javafx.scene.layout.VBox;
+import java.util.Scanner;
+import java.io.File;
 
 public class SudokuGame implements Playable {
     
     private Stage stage;
-    private int[] fullGameData;
+    private Tile[][] fullGameData;
+    private Tile[][][] premadeBoards;
     
     public void play() {
-	
-	HBox hbox = new HBox();
-	fullGameData = new int[81];
-	for(int i = 0; i < 81; i++){ // TO TEST
-	    fullGameData[i] = 0;
-	} // for
-	int[] temp = new int[9];
-	for(int q = 0; q > temp.length; q++){
-	    temp[q] = 0;
-	} // for TEMPORARY
-	BoardPiece bp = new BoardPiece(temp);
-	hbox.getChildren().add(bp);
+
+	premadeBoards = makePremadeBoards();
+	fullGameData = createGameData();
+	HBox row1 = makeRowOne();
+	HBox row2 = makeRowTwo();
+	HBox row3 = makeRowThree();
+	VBox vbox = new VBox();
+	vbox.getChildren().addAll(row1, row2, row3);
 	stage = new Stage();
-	Scene scene = new Scene(hbox);
+	Scene scene = new Scene(vbox, Color.BLACK);
 	stage.setScene(scene);
+	stage.setTitle("Sudoku!");
 	stage.sizeToScene();
 	stage.show();
 	
     } // play
+
+    private Tile[][] createGameData(){
+	Tile[][] tiles = new Tile[9][9];
+	for(int i = 0; i < 9; i++){
+	    for(int j = 0; j < 9; j++){
+		tiles[i][j] = new Tile(0);
+	    } // for
+	} // for
+	return tiles;
+    } // createGameData
+
+    private HBox makeRowOne(){
+	HBox hbox = new HBox();
+	BoardPiece bp0 = makeBoardPiece(0);
+	BoardPiece bp1 = makeBoardPiece(1);
+	BoardPiece bp2 = makeBoardPiece(2);
+	hbox.getChildren().addAll(bp0, bp1, bp2);
+	return hbox;
+    } // makeRowOne
+
+    private HBox makeRowTwo(){
+	HBox hbox = new HBox();
+	BoardPiece bp3 = makeBoardPiece(3);
+	BoardPiece bp4 = makeBoardPiece(4);
+	BoardPiece bp5 = makeBoardPiece(5);
+	hbox.getChildren().addAll(bp3, bp4, bp5);
+	return hbox;
+    } // makeRowTwo
+
+    private HBox makeRowThree(){
+	HBox hbox = new HBox();
+	BoardPiece bp6 = makeBoardPiece(6);
+	BoardPiece bp7 = makeBoardPiece(7);
+	BoardPiece bp8 = makeBoardPiece(8);
+	hbox.getChildren().addAll(bp6, bp7, bp8);
+	return hbox;
+    } // makeRowThree
+    
+    private BoardPiece makeBoardPiece(int num){
+	int row, col;
+	Tile[] tiles = new Tile[9];
+	int index = 0;
+	for(int i = 0; i < 3; i++){
+	    row = i + (3 * (num / 3));
+	    for(int j = 0; j < 3; j++){
+		col = j + (3 * (num % 3));
+		tiles[index] = fullGameData[row][col];
+		index++;
+	    } // for
+	} // for
+	BoardPiece bp = new BoardPiece(tiles);
+	return bp;
+    } // makeBoardPiece
+    
+    private Tile[][][] makePremadeBoards(){
+	Tile[][][] premade = new Tile[4][9][9];
+	try{
+	    File board1 = new File("cs1302/arcade/boards/board1.txt");
+	    Scanner scan = new Scanner(board1);
+	    Tile t;
+	    for(int i = 0; i < 9; i++){
+		for(int j = 0; j < 9; j++){
+		    t = new Tile(Integer.parseInt(scan.nextLine()));
+		    premade[0][i][j] = t;
+		} // for
+	    } // for
+	} catch(Exception e){
+	    System.out.println("Something went wrong!");
+	} // catch
+	return premade;
+    } // makePremadeBoards
     
 } // SudokuGame
